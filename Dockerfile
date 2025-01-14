50dae07c8a32094d43906e489ffdcf11aecc2fa3
wendongzheng <wendongzheng@quicktron.com>
2025-01-14 14:37:41 +0800

    util

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,13 @@ RUN CGO_ENABLED=0 go build -v -o main .
 FROM ubuntu:24.10
 
 RUN apt-get update \
-    && apt-get install -y python3 wget \
+    && apt-get install -y python3 wget nodejs npm\
     && apt-get install -y fonts-wqy-zenhei fonts-wqy-microhei \
-    && apt-get install -y libegl1  libopengl0 libxcb-cursor0 libfreetype6 xz-utils chromium-browser
+    && apt-get install -y libegl1  libopengl0 libxcb-cursor0 libfreetype6 xz-utils qt6-base-dev
 #RUN wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin
+
 COPY calibre.txz /opt/calibre/calibre.txz
-RUN cd /opt/calibre && xz -d calibre.txz
+RUN cd /opt/calibre && tar -xf calibre.txz 
 
 ENV PATH="$PATH:/opt/calibre/"
 ENV TZ=Asia/Shanghai
@@ -25,6 +26,9 @@ VOLUME [ "/app/uploads" ]
 
 WORKDIR /app
 
+COPY package.json ./package.json
+RUN npm install cnpm && cnpm install puppeteer@2.0
+
 COPY --from=base /app/main ./bookstack
 
 COPY conf ./conf
@@ -32,6 +36,6 @@ COPY dictionary ./dictionary
 COPY static ./static
 COPY views ./views
 
-RUN ./bookstack install
+#RUN ./bookstack install
 
 CMD ["./bookstack"]
--- epub/epub_test.go
+++ epub/epub_test.go
@@ -3,6 +3,7 @@ package epub
 import (
 	"log"
 	"os"
+	"path"
 	"testing"
 	"time"
 
@@ -67,6 +68,56 @@ func TestImportEpub(t *testing.T) {
 		ds := models.NewDocumentStore()
 		ds.DocumentId = int(docIdInt64)
 		ds.Markdown = markdown
+		ds.Content = string(htmlContent)
+		ds.InsertOrUpdate(*ds, "markdown")
+	}
+}
+
+func TestImportBookDir(t *testing.T) {
+	commands.ConfigurationFile = "../conf/app.conf"
+	commands.ResolveCommand(os.Args)
+	bookName := "A-B测试从0到1"
+	bookPath := "C:\\workspace\\geektime-docs\\AI-大数据\\A-B测试从0到1\\docs"
+
+	book := models.NewBook()
+	book.BookName = bookName
+	book.ParentId = 0
+	book.Identify = "ai-test-0-1"
+	book.Author = "管理员"
+	book.PrivatelyOwned = 1
+	book.MemberId = 1
+	book.Editor = "markdown"
+	book.Theme = "default"
+	book.Version = time.Now().Unix()
+	book.ReleaseTime = time.Now()
+	book.GenerateTime = time.Now()
+	book.LastClickGenerate = time.Now()
+
+	book.Insert()
+
+	bookResult, err := models.NewBookResult().FindByIdentify(book.Identify, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bookId := bookResult.BookId
+	bk, _ := os.Open(bookPath)
+	chs, _ := bk.Readdir(0)
+	for _, v := range chs {
+		markdown, _ := os.ReadFile(path.Join(bookPath, v.Name()))
+		doc := models.NewDocument()
+		doc.BookId = bookId
+		doc.MemberId = 1
+		doc.ParentId = 0
+		doc.DocumentName = v.Name()
+		doc.Markdown = string(markdown)
+		doc.Identify = time.Now().Format("20060102150405.000")
+		doc.Version = time.Now().Unix()
+		htmlContent := MarkdownToHtml([]byte(markdown))
+		doc.Release = string(htmlContent)
+		docIdInt64, _ := doc.InsertOrUpdate()
+		ds := models.NewDocumentStore()
+		ds.DocumentId = int(docIdInt64)
+		ds.Markdown = string(markdown)
 		ds.Content = string(htmlContent)
 		ds.InsertOrUpdate(*ds, "markdown")
 	}
